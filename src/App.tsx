c22b0cc49f7848c9b9294e4e4a7a4abbef0fa421
Eliabe Gai <eliabe.gai@clinicorp.com>
2024-01-01 22:14:04 -0300

    chore: Alterações e limpeza do código geral

--- src/App.tsx
+++ src/App.tsx
@@ -29,105 +29,6 @@ type GetItensProps = {
 export default function App() {
 
   const [open, setOpen] = useState(false);
- 
-  const handleOpen = () => setOpen(!open);
-
-  const [name, setName] = useState('');
-  const [idade, setIdade] = useState('');
-
-  const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value)
-  }
-  const handleIdade = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIdade(event.target.value)
-  }
-
-  const clearForm = () => {
-    handleOpen()
-    setName('')
-    setIdade('')
-    setNameAnime('')
-    setTypeAnime('')
-    setChapterAnime(0)
-    setStatusAnime('')
-    setScanAnime('')
-    setScanUrlAnime('')
-  }
-
-  // const handleSubmit = ({ name, idade }:handleSubmitProps) => {
-    
-  //   const data = {
-  //     "nome": name,
-  //     "idade": idade
-  //   }
-
-  //   fetch("http://localhost:3000/users", {
-  //     method: "POST",
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(data),
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //       clearForm()
-  //     });
-
-  // }
-
-  // const handleClick = () => {
-  //   const data = {
-  //     "name": name,
-  //     "idade": idade
-  //   }
-
-  //   handleSubmit(data)
-  // }
-
-  const handleClickAnime = async () => {
-
-    const url = await newPostImage()
-
-    const data = {
-      "name": nameAnime,
-      "type": typeAnime,
-      "chapter": chapterAnime,
-      "status": statusAnime,
-      "scan": {
-        "name": scanAnime,
-        "url": scanUrlAnime
-      },
-      "imageUrl": url
-    }
-
-    await fetch("http://localhost:3000/animes", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        clearForm()
-      });
-  }
-
-  const handleTeste = () => {
-    handleClickAnime()
-  };
-
-
-  useEffect(() => {
-    setNameAnime('')
-    setTypeAnime('')
-    setChapterAnime(0)
-    setStatusAnime('')
-    setScanAnime('')
-    setScanUrlAnime('')
-  },[])
-
   const [nameAnime, setNameAnime] = useState('')
   const [typeAnime, setTypeAnime] = useState('')
   const [chapterAnime, setChapterAnime] = useState(0)
@@ -135,6 +36,27 @@ export default function App() {
   const [scanAnime, setScanAnime] = useState('')
   const [scanUrlAnime, setScanUrlAnime] = useState('')
   const [file, setFile] = useState<File>()
+  const [openImage, setOpenImage] = useState(false)
+  const [items, setItems] = useState<GetItensProps[]>([])
+  const [loading, setLoading] = useState(false)
+
+  
+  const handleOpen = () => setOpen(!open);
+  const handleOpenImage = () => setOpenImage(!openImage)
+
+  useEffect(() => {
+    getData()
+  },[loading])
+
+  const clearForm = () => {
+    handleOpen()
+    setNameAnime('')
+    setTypeAnime('')
+    setChapterAnime(0)
+    setStatusAnime('')
+    setScanAnime('')
+    setScanUrlAnime('')
+  }
 
   const optionsStatus = [
     {
@@ -174,7 +96,6 @@ export default function App() {
     event.preventDefault()
     if(type === 'nameAnime') {
     }
-    
     switch(type) {
       case 'nameAnime':
         setNameAnime(event.target.value);
@@ -221,13 +142,7 @@ export default function App() {
       setItems(data);
   }
 
-  const [openImage, setOpenImage] = useState(false)
-
-  const handleOpenImage = () => setOpenImage(!openImage)
-
-  const [items, setItems] = useState<GetItensProps[]>([])
-
-  const newPostImage = async () => {
+  const postImage = async () => {
     
     if(file && file.size > 0) {
       
@@ -242,9 +157,38 @@ export default function App() {
     }
   }
 
-  useEffect(() => {
-    getData()
-  },[open])
+  const handleClickAnime = async () => {
+
+    const url = await postImage()
+
+    const data = {
+      "name": nameAnime,
+      "type": typeAnime,
+      "chapter": chapterAnime,
+      "status": statusAnime,
+      "scan": {
+        "name": scanAnime,
+        "url": scanUrlAnime
+      },
+      "imageUrl": url
+    }
+
+    await fetch("http://localhost:3000/animes", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        clearForm()
+      });
+  }
+
+  const handleTeste = () => {
+    handleClickAnime()
+  };
 
   return (
     <div>
@@ -252,39 +196,8 @@ export default function App() {
 
       <div>
         <div className="flex bg-gray-600 justify-center items-center p-4">
-          <button className="w-52 h-20 border-2 border-green-500 text-cyan-500 text-3xl" onClick={handleOpen}>Open Modal</button>
+          <button className="w-52 h-20 border-2 border-green-500 text-cyan-500 text-3xl" onClick={handleOpen}>New Item</button>
         </div>
-        {/* <div className="flex bg-gray-600 justify-center items-center p-4">
-          <button className="w-52 h-20 border-2 border-green-500 text-cyan-500 text-3xl" onClick={handleOpenImage}>GET DATA</button>
-        </div> */}
-
-        {/* {
-          openImage &&
-          <Modal
-            openModal={openImage}
-            handleOpen={handleOpenImage}
-            onConfirm={newPostImage}
-          >
-            <input type="file" placeholder="teste" onChange={handleSaveFile} name="arquivo" />
-          </Modal>
-        } */}
-
-        {/* {
-          open &&
-            <Modal
-            openModal={open}
-            handleOpen={handleOpen}
-            onConfirm={handleClick}
-            >
-              <FormUser
-                name={name}
-                idade={idade}
-                handleIdade={handleIdade}
-                handleName={handleName}
-                handleSubmit={handleSubmit}
-              />
-            </Modal>
-        } */}
 
         <Modal
           handleOpen={handleOpen}
@@ -306,24 +219,8 @@ export default function App() {
           </div>
         </Modal>
 
-        {/* <div className="flex w-full flex-wrap h-[95vh] bg-gray-800 text-white justify-center items-center border-2 border-green-200 gap-1 overflow-auto p-2">
-        {
-          listAnimes.map((i, index) => (
-            <Card
-              key={i.id}
-              name={i.name}
-              chapter={i.chapter}
-              status={i.status}
-              image={i.image}
-              type={i.type}
-              scans={i.scans}
-            />
-          ))
-        }
-
-        </div> */}
         <div className="flex w-full flex-wrap h-[95vh] bg-gray-800 text-white justify-center items-center border-2 border-green-200 gap-1 overflow-auto p-2">
-      {
+          {
             items &&
             items.map(i => (
               <Card
@@ -336,7 +233,7 @@ export default function App() {
                 newScans={i.scan}
               />
             ))
-        }
+          }
         </div>
 
       </div>
