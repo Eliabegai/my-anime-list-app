f8b13a481530e551dd2c238a17da87e115499340
Eliabe Gai <eliabe.gai@clinicorp.com>
2024-01-25 13:34:26 -0300

    remove: button toltip

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -25,14 +25,6 @@ export const Card = ({name, status, type, chapter, image, scans, newScans, id, h
         <ul>
           <li className="w-36">
             <p className="text-lg font-bold truncate overflow-hidden"> {name || 'Undefined'} </p>
-
-                <button data-tooltip-target="tooltip-default" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Default tooltip</button>
-
-                <div id="tooltip-default" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                    Tooltip content
-                    <div className="tooltip-arrow" data-popper-arrow></div>
-                </div>
-
           </li>
           <li>
             <a href={newScans?.url} className="border-b-2 border-cyan-400 text-cyan-400" target="_blank" rel='noopener' >{newScans?.name} </a>
