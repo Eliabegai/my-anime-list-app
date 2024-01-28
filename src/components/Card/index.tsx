322244a75a4aac35043bae2b25a5593153afd09e
Eliabe Gai <eliabe.gai@clinicorp.com>
2024-01-28 16:02:44 -0300

    change: alterado id de number para string

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,32 +4,80 @@ import { CardProps } from "../../types/listAnimesProps";
 import { Button } from "../Button";
 import { Tooltip } from "../Tolltip";
 
-export const Card = ({name, status, type, chapter, image, scans, newScans, id, handleButtonChangeAdd, handleButtonChangeRemove, handleChangeChapter }:CardProps) => {
-  const [ value, setValue ] = useState(chapter || 0)
+export const Card = ({name, status, type, chapter, image, newScans, _id, handleButtonChangeAdd, handleButtonChangeRemove, handleChangeChapter }:CardProps) => {
+  const [ value, setValue ] = useState(chapter || '')
 
   const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if(event.key === 'Enter') {
       if(typeof handleChangeChapter === 'function')
-      handleChangeChapter(value, id as number)
+      handleChangeChapter(value, _id as string)
     }
   }
 
   const handleModifyChapter = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(parseFloat(event.target.value))
+    const changeValue = event.target.value
+    setValue(changeValue)
   }
 
+
   return (
-    <div className="border-2 border-green-400 flex flex-wrap w-80 p-2 items-center">
-      <img src={image} alt={name} className="w-32 h-40"/>
-      <div className="ml-4">
-        <ul>
+    <div className="box-content flex flex-col w-60 h-80 border-2 border-green-400 p-2 items-center">
+      
+      <div className="box-content max-w-40 max-h-44 p-2">
+        <img src={image} alt={name} className="w-32 h-40"/>
+      </div>
+      
+      <div className="box-content flex flex-col w-56 h-auto ml-2 p-2">
+
+        <span className="">
+        <Tooltip label={name || 'Undefined'}>
+          <p className="text-base font-semibold truncate overflow-hidden"> {name || 'Undefined'} </p>
+        </Tooltip>
+        </span>
+        
+        <span>
+          <a href={newScans?.url} className="border-b border-cyan-400 text-cyan-400 text-sm" target="_blank" rel='noopener' >{newScans?.name} </a>
+        </span>
+
+        <span className="text-sm">
+          Status: {status}
+        </span>
+
+        <span className="text-sm">
+          Type: {type}
+        </span>
+        <div className="box-content flex flex-row gap-x-1">
+
+          <span className="text-sm">
+            Chapter:
+          </span>
+            <span className="text-base">
+              <input 
+              className="outline-none border border-green-300 text-center rounded w-20 bg-transparent" 
+              type="text" 
+              value={value} 
+              onChange={handleModifyChapter}
+              onKeyDown={onKeyDown}
+              placeholder="chapter" 
+              />
+            </span>
+
+          <div className="flex justify-around">
+            <Button onClick={handleButtonChangeRemove as () => void} size="small">-</Button>
+            <Button onClick={handleButtonChangeAdd as () => void} size="small">+</Button>
+          </div>
+
+        </div>
+
+        
+        {/* <ul>
           <li className="w-36">
             <Tooltip label={name || 'Undefined'}>
-              <p className="text-lg font-bold truncate overflow-hidden"> {name || 'Undefined'} </p>
+              <p className="text-base font-semibold truncate overflow-hidden"> {name || 'Undefined'} </p>
             </Tooltip>
           </li>
           <li>
-            <a href={newScans?.url} className="border-b-2 border-cyan-400 text-cyan-400" target="_blank" rel='noopener' >{newScans?.name} </a>
+            <a href={newScans?.url} className="border-b border-cyan-400 text-cyan-400 text-sm" target="_blank" rel='noopener' >{newScans?.name} </a>
           </li>
           <li>
             Status: {status}
@@ -39,9 +87,9 @@ export const Card = ({name, status, type, chapter, image, scans, newScans, id, h
           </li>
           <li>
             Chapter:
-            <span className="flex justify-center items-centers">
+            <span className="">
               <input 
-              className="outline-none border-2 border-green-600 rounded w-24 flex justify-center items-center bg-transparent text-center" 
+              className="outline-none border border-green-600 rounded w-24 flex justify-center items-center bg-transparent text-center" 
               type="text" 
               value={value} 
               onChange={handleModifyChapter}
@@ -50,11 +98,15 @@ export const Card = ({name, status, type, chapter, image, scans, newScans, id, h
               />
             </span>
           </li>
-        </ul>
-        <div className="flex justify-around">
+        </ul> */}
+        
+        {/* 
+        <div className="container border flex justify-around p-1">
           <Button onClick={handleButtonChangeRemove as () => void} size="small">-</Button>
           <Button onClick={handleButtonChangeAdd as () => void} size="small">+</Button>
         </div>
+         */}
+
       </div>
     </div>
   )
